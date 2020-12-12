0b6492cf620d165d34eb048b232b8dd5f620ce72
NguyenCong176708 <conghoahoa1cnh@gmail.com>
2020-12-13 00:10:11 +0700

    ok

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,11 +3,27 @@ class UsersController < ApplicationController
     @users = User.search(params[:term])
     respond_to :js
   end
-
+  def following
+    @title = "Following"
+    @user = User.find(params[:id])
+    @users = @user.following.paginate(:page => params[:page], :per_page => 6)
+    render 'show_follow'
+  end
+  def followers
+    @title = "Followers"
+    @user = User.find(params[:id])
+    @users = @user.followers.paginate(:page => params[:page], :per_page => 6)
+    render 'show_follow'
+  end
   def show
     @user = User.find(params[:id])
     @posts = @user.posts.includes(:photos, :likes, :comments)
-    @saved = Post.joins(:bookmarks).where(user_id: current_user.id).
+    @saved = Post.joins(:bookmarks).where("bookmarks.user_id = ?", current_user.id).
       includes(:photos, :likes, :comments) if @user == current_user
+  end
+
+  def list_user
+    @users = User.all.paginate(:page => params[:page], :per_page => 6)
+    render "list_user"
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   has_many :likes, dependent: :destroy
   has_many :comments, dependent: :destroy
   has_many :bookmarks, dependent: :destroy
+  
   has_many :active_relationships, class_name: "Relationship",
                                   foreign_key: "follower_id",
                                   dependent: :destroy
@@ -12,6 +13,11 @@ class User < ApplicationRecord
   has_many :following, through: :active_relationships, source: :followed
   has_many :followers, through: :passive_relationships, source: :follower
 
+  def feed
+    following_ids = "SELECT followed_id FROM relationships WHERE follower_id = :user_id"
+    Post.where("user_id IN (#{following_ids}) OR user_id = :user_id", user_id: id)
+  end
+  
   def follow(other_user)
     following << other_user
   end
