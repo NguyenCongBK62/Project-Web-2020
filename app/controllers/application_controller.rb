2b771548cb75e689041f05417f0a80a078c5e08e
NguyenCong176708 <conghoahoa1cnh@gmail.com>
2020-12-12 18:24:23 +0700

    ok

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,12 @@
+class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :avatar, :avatar_cache])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :website, :bio, :avatar, :avatar_cache, :remove_avatar])
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,25 @@
+Rails.application.routes.draw do
+  root 'posts#index'
+
+  devise_for :users,
+    path: '',
+    path_names: {sign_in: 'login', sign_out: 'logout', edit: 'profile', sign_up: 'registration'},
+    controllers: {omniauth_callbacks: 'omniauth_callbacks', registrations: 'registrations'}
+
+  # get '/users/:id', to: 'users#show'
+  # /users/3 -> Users controller, show action, params {id: '3'}
+  resources :users, only: [:index, :show] do 
+    member do
+      get :following, :followers
+    end
+  end
+
+
+  resources :posts, only: [:index, :show, :create, :destroy] do
+    resources :photos, only: [:create]
+    resources :likes, only: [:create, :destroy], shallow: true
+    resources :comments, only: [:index, :create, :destroy], shallow: true
+    resources :bookmarks, only: [:create, :destroy], shallow: true
+  end
+  resources :relationships, only: [:create, :destroy]
+end
